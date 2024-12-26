af2499b291dc4446fc6382311065d5a78c29285e
jeong <jw.jeong@tilda.co.kr>
2024-12-26 12:21:30 +0900

    :heavy_plus_sign: typeorm 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { LectureModule } from './lecture/lecture.module';
 import { EnrollmentModule } from './enrollment/enrollment.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfigService } from './configs/config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const domainModules = [LectureModule, EnrollmentModule];
 
 @Module({
   imports: [
     ConfigModule.forRoot(AppConfigService.getConfigModuleOptions()),
+    TypeOrmModule.forRootAsync(AppConfigService.getTypeOrmModuleConfigs()),
     ...domainModules,
   ],
   controllers: [AppController],
--- src/configs/config.service.ts
+++ src/configs/config.service.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModuleOptions } from '@nestjs/config';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 import * as path from 'path';
+import { DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({})
 export class AppConfigService {
@@ -16,6 +23,37 @@ export class AppConfigService {
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
       }),
+    };
+  }
+
+  static getTypeOrmModuleOptions(
+    configService: ConfigService,
+  ): DataSourceOptions {
+    const dbConfig: DataSourceOptions = {
+      type: 'mysql',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME'),
+      synchronize: false,
+      logging: true,
+      entities: ['dist/**/domain/*.entity.{ts,js}'],
+      migrations: ['dist/migrations/**/*{.ts,.js}'],
+      namingStrategy: new SnakeNamingStrategy(),
+      migrationsRun: true,
+    };
+
+    return dbConfig;
+  }
+
+  static getTypeOrmModuleConfigs(): TypeOrmModuleAsyncOptions {
+    return {
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...this.getTypeOrmModuleOptions(configService),
+      }),
+      inject: [ConfigService],
     };
   }
 }
